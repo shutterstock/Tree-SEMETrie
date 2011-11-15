b3fc7caaa8423e40968f8f9e3e14b598908e3c89
aaron cohen <aaron@aarondcohen.(none)>
2011-11-15 02:18:54 -0500

    - bumped version

--- lib/Tree/SEMETrie.pm
+++ lib/Tree/SEMETrie.pm
@@ -13,11 +13,11 @@ Tree::SEMETrie - Single-Edge Multi-Edge Trie
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 #Class Constants
 my $VALUE  = 0;
